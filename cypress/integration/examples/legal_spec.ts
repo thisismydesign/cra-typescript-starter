ffd37d0e219b436d04db099b3ab0d6c41737e863
Csaba Apagyi <csaba.apagyi@gmail.com>
2020-09-01 17:25:42 +0200

    Fix e2e specs

--- cypress/integration/examples/legal_spec.ts
+++ cypress/integration/examples/legal_spec.ts
@@ -1,6 +1,6 @@
 describe("/legal", () => {
   it("contains terms and conditions, imprint, and privacy policy", () => {
-    cy.visit("/legal", { qs: { env: "CI" } });
+    cy.visit("/cra-typescript-starter/#/legal", { qs: { env: "CI" } });
     cy.contains("Terms and Conditions");
     cy.contains("Imprint");
     cy.contains("Privacy Policy");
