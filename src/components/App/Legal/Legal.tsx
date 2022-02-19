82f86cc55d88f5c69c9287fc5451a555f75e9382
Csaba Apagyi <csaba.apagyi@gmail.com>
2022-02-19 18:15:56 +0100

    Update react-markdown to a new version that doesn't need a polyfill

--- src/components/App/Legal/Legal.tsx
+++ src/components/App/Legal/Legal.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from "react";
-// import ReactMarkdown from "react-markdown";
+import ReactMarkdown from "react-markdown";
 import raw from "raw.macro";
 import { Box, Container } from "@material-ui/core";
 import { Helmet } from "react-helmet";
@@ -12,7 +12,7 @@ const Legal: FunctionComponent = () => {
       </Helmet>
       <Box p={1} pt={[2, 4]} pb={[4, 8]}>
         <Container>
-          {/* <ReactMarkdown source={raw("../../../resources/legal.md")} /> */}
+          <ReactMarkdown>{raw("../../../resources/legal.md")}</ReactMarkdown>
         </Container>
       </Box>
     </>
