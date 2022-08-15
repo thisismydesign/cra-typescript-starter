db7ce34973809a687a785f3e664332c4a9018c0b
thisismydesign <csaba.apagyi@gmail.com>
2022-08-15 10:55:13 +0000

    Deploying to gh-pages from @ thisismydesign/cra-typescript-starter@a91a6be97f3693f42289d41209e5d0273de02ee5 🚀

--- static/js/880.6c9fd2ae.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcra_typescript_starter=self.webpackChunkcra_typescript_starter||[]).push([[880],{8880:function(n,r,e){e.r(r);e(2791);var t=e(2619),s=e(4554),c=e(6445),i=e(4270),a=e(184);r.default=function(){return(0,a.jsxs)(a.Fragment,{children:[(0,a.jsx)(i.q,{children:(0,a.jsx)("meta",{name:"robots",content:"noindex"})}),(0,a.jsx)(s.Z,{p:1,pt:[2,4],pb:[4,8],children:(0,a.jsx)(c.Z,{children:(0,a.jsx)(t.D,{children:"# Imprint\n\n# Privacy Policy\n\n# Terms and Conditions\n"})})})]})}}}]);
-//# sourceMappingURL=880.6c9fd2ae.chunk.js.map
